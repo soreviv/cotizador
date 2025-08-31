6287b3766b87ce6bf985e0261b89f0797fc6e176
Alejandro Viveros Dominguez <alexviveros@pm.me>
2025-08-31 16:13:23 -0600

    cambios php

--- generar.php
+++ generar.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     echo "<h1>DEBUGGING SESSION</h1>";
